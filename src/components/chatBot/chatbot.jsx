ea6edc148573d26eb42c37353405f54158c2c3a6
Rocketman <abhinayshambharkar1@gmail.com>
2025-03-03 23:56:32 +0530

    prepare environment to add chatBot

---
+++ src/components/chatBot/chatbot.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { TbMessageChatbot } from "react-icons/tb";
+
+
+const chatbot = () => {
+  return (
+    <div className='chatbot'>
+      <TbMessageChatbot />
+    </div>
+  )
+}
+
+export default chatbot
